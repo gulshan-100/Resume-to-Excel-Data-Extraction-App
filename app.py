44f54b8ae5652485aa2de791232c86b5de00fb35
Gulshan Kumar <132364518+gulshan-100@users.noreply.github.com>
2024-04-13 22:11:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ import streamlit as st
 import pandas as pd
 import docx2txt 
 import re  
+import pandas as pd 
 from openpyxl.workbook import Workbook
 from docx import Document  # For processing DOCX files
 from PyPDF2 import PdfReader  # For processing PDF files
+from io import BytesIO
+
+
 
 def main():
     st.title("CV Parser Application")
@@ -21,12 +25,21 @@ def main():
     df = pd.DataFrame(textual_data)
     # st.write(df)
     
+    # Create a BytesIO object
+    output = BytesIO()
+    excel_file_name = 'data.xlsx'
+
+    # Write DataFrame to the BytesIO object
+    df.to_excel(output, index=False)
+
+    # Retrieve the data from the BytesIO object as bytes
+    excel_file = output.getvalue()
+
     # Add a button to download the Excel file
-    if st.button('Download Excel'):
-        excel_file_name = "cv_data.xlsx"
-        df.to_excel(excel_file_name, index=False)
+    if st.download_button(label="Download Excel", data=excel_file, file_name=excel_file_name, mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"):
         st.success("Excel file created successfully: {}".format(excel_file_name))
 
+    
 def extract_text_from_cv(cv):
     data_list = []
     
@@ -46,17 +59,12 @@ def extract_text_from_cv(cv):
     emails = re.findall(email_pattern,text)
     phones  = re.findall(phone_pattern, text)
     
+    # Add overall text
+    overall_text = text
     
-    # Flatten the lists and remove square brackets
-    emails = ', '.join(emails)
-    phones = ', '.join(phones)
-    
-    data_list.append({"Email": emails, "Phone": phones})
+    data_list.append({"Email": ', '.join(emails), "Phone": ', '.join(phones), "Overall Text": overall_text})
+   
     return data_list
-
-
-
-
 
 
 if __name__ == '__main__':
